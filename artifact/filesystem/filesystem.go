d80ca4f5f5afb81b197ca922bd14cbea8e18aa75
panbin <panbin@codeck.ai>
2026-08-30 23:28:20 +0800

    fix: remove lifecycle implementation compatibility exports

--- artifact/filesystem/filesystem.go
+++
@@ -1,9 +0,0 @@
-// Package filesystem exposes Lifecycle-owned filesystem artifact adapters.
-// Implementations remain internal to the Lifecycle source module.
-package filesystem
-
-import internal "github.com/domainry/domainry-lifecycle/internal/infrastructure/artifact/filesystem"
-
-type SubjectStore = internal.SubjectStore
-
-func NewSubjectStore(root string) *SubjectStore { return internal.NewSubjectStore(root) }
